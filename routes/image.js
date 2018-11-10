c47e90ef76be320c0d3e3a4031f575673e7a7de3
Shivam Kaushik <shivam.kaushik73@gmail.com>
2018-11-10 18:35:12 +0530

    File now saving in filesystem

--- routes/image.js
+++ routes/image.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
 
+// for appending extention
+var storage = multer.diskStorage({
+	destination: function(req, file, cb) {
+		cb(null, 'uploads');
+	},
+	filename: function(req, file, cb) {
+		cb(null, Date.now() + '.' + file.originalname.split('.').pop());
+	}
+});
+
+var upload = multer({ storage: storage });
 /* POST users listing. */
-router.post('/', function(req, res, next) {
+router.post('/', upload.single('avatar'), function(req, res, next) {
+	console.log(
+		'Now fork the python process and call it with arguments grader.py i=' +
+			req.file.path
+	);
 	res.send('respond with a resource');
-	// eslint-disable-next-line no-console
-	console.log(req.params);
 });
 
 module.exports = router;
