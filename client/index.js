da0fb1cf2a8f028d22f806d65f3ae8034438e5ae
Shivam Kaushik <shivam.kaushik73@gmail.com>
2018-11-18 18:50:30 +0530

    Change design
    - New index page
    - New color palette
    - Add new image to index page
    - Add submit button

--- client/index.js
+++ client/index.js
@@ -10,8 +10,6 @@ window.$ = jquery;
 const materialize = require('materialize-css');
 const materializeCss = require('../node_modules/materialize-css/dist/css/materialize.min.css');
 
-// capture.js
-const capture = require('./capture');
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.modal');
 	// eslint-disable-next-line no-undef
@@ -22,5 +20,21 @@ document.addEventListener('DOMContentLoaded', function() {
 		indicators: true,
 		duration: 0
 	});
-	capture();
+
+	// for displaying image on selection
+	function showImage(src, target) {
+		var fr = new FileReader();
+		// when image is loaded, set the src of the image where you want to display it
+		fr.onload = function(e) {
+			target.src = this.result;
+		};
+		src.addEventListener('change', function() {
+			// fill fr with image data
+			fr.readAsDataURL(src.files[0]);
+		});
+	}
+
+	var src = document.getElementById('upload-btn');
+	var target = document.getElementById('placeholder-img');
+	// showImage(src, target);
 });
